e9b130625ab4cfcf83233c6746d567c07f0b0619
gianninapg@gmail.com <Gi@090488>
2020-11-02 20:23:36 +1000

    css changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import Logo from "../components/Logo/Logo.png";
 function Nav(){
     return(
         <nav className="nav">
-            <img src={Logo} className="Logo" />
+            <img src={Logo} alt="logo" className="Logo" />
             <Link className="linkLogin" to="/login">Login</Link>
             <Link className="link" to="/">Home</Link>
             <Link className="link" to ="/pledges">Support Projects</Link>
