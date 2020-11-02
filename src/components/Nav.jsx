2488a55062b7477676940dfcc120bb6d3a071ea8
gianninapg@gmail.com <Gi@090488>
2020-11-02 19:02:11 +1000

    logo created and added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../components/Logo/Logo.png";
 
 function Nav(){
     return(
         <nav className="nav">
+            <img src={Logo} className="Logo" />
             <Link className="linkLogin" to="/login">Login</Link>
             <Link className="link" to="/">Home</Link>
             <Link className="link" to ="/pledges">Support Projects</Link>
